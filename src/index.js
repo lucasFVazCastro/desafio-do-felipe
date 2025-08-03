1ba322cc731fb1a7c273786307cbb5113e819e6e
lucasFVazCastro <lucasfernandesvazdecastro03@gmail.com>
2025-08-02 22:21:39 -0300

    Projeto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-console.log("Esta funcionando")
+let nomeHeroi = "Reikkan"
+let xp = 1156
+let nivel = ""
 
+if (xp <= 1000) {
+    nivel = "Ferro";
+    
+}else if (xp <= 2000){
+    nivel = "Bronze";
+    
+}else if (xp <= 5000){
+    nivel = "Prata";
+   
+}else if (xp <= 7000){
+    nivel = "Ouro";
+  
+}else if (xp <= 8000){
+    nivel = "Platina";
+    
+}else if (xp <= 9000){
+    nivel = "Ascendente";
+   
+}else if (xp <= 10000){
+    nivel = "Imortal";
+    
+}else{
+    nivel = "Radiante";
+    
+}
+console.log("O Herói chamado de " + nomeHeroi + " está no nível de " + nivel)
